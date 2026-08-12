187fe26c4ab31861422433a2f78c9138ee711b1c
annaluizarossi962pditabiracom-afk <annaluizarossi962pditabira.com@gmail.com>
2026-08-12 20:07:12 -0300

    Add files via upload

---
+++ .script.py
@@ -0,0 +1,31 @@
+Ex
+Entrada
+print("Meu primeiro codigo")
+Saida esperada
+Meu primeiro codigo
+
+x = 10
+
+y = 5
+
+print(x+1)
+
+print(y-2)
+
+print(y*x)
+
+print(x/y)
+
+11
+
+3 
+
+50 
+
+2.0
+
+print("Ola) 
+print("Ola) 
+ SyntaxError: unterminated string literal (detected at line 1)
+
+
